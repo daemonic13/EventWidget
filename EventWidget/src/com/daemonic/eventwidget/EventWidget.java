57ddbad3674d8d0c1a17d805e0b4b9e16d935210
cdbou <cdbou@cdbvista-12351.csi.campana.com>
2012-08-24 12:51:41 -0400

    Update to get it working!

--- EventWidget/src/com/daemonic/eventwidget/EventWidget.java
+++ EventWidget/src/com/daemonic/eventwidget/EventWidget.java
@@ -20,19 +20,29 @@ public class EventWidget extends AppWidgetProvider {
 		// Get all ids
 	    ComponentName thisWidget = new ComponentName(context,
 	    		EventWidget.class);
+	    Log.w("eventwidget","started");
 	    int[] allWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
 	    for (int widgetId : allWidgetIds) {
+	    	Log.w("eventwidget",Integer.toString(widgetId));
 	      // Create some random data
 	      RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
 	          R.layout.event_widget);
 	      
-	      // Set the text
-	      //final LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-	      //TextView tv = (TextView)inflater.inflate(R.layout.event_item, null);;
+	      // Set the text for 3 items...?
 	      RemoteViews tv = new RemoteViews(context.getPackageName(),R.layout.event_item);
 	      tv.setTextViewText(R.id.event_item_text,"Test Case");
 	      Log.w("eventwidget",tv.toString());
-	      remoteViews.addView(R.id.event_widget_body,tv);
+	      remoteViews.addView(R.id.event_widget_scroll,tv);
+	      
+	      RemoteViews tv2 = new RemoteViews(context.getPackageName(),R.layout.event_item);
+	      tv2.setTextViewText(R.id.event_item_text,"Test Case 2");
+	      Log.w("eventwidget",tv2.toString());
+	      remoteViews.addView(R.id.event_widget_scroll,tv2);
+	      
+	      RemoteViews tv3 = new RemoteViews(context.getPackageName(),R.layout.event_item);
+	      tv3.setTextViewText(R.id.event_item_text,"Test Case 3");
+	      Log.w("eventwidget",tv3.toString());
+	      remoteViews.addView(R.id.event_widget_scroll,tv3);
 
 	      // Register an onClickListener
 	      Intent intent = new Intent(context, EventWidget.class);
